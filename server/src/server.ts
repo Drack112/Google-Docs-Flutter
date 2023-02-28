cd602d932383efc825d5faaf8e13b5ea2271dbc7
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-28 00:28:40 -0300

    :computer: setup the base server with express

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+import "express-async-errors";
+import "dotenv/config";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.APP_PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+})
