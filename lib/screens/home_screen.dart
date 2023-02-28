080b360b6c0653d3c0e3154007dc39fb998aeda6
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-28 14:24:24 -0300

    :iphone: Home Screen with Consumer on authRepository

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:docs_google/repository/auth_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          ref.watch(userProvider)!.email,
+        ),
+      ),
+    );
+  }
+}
