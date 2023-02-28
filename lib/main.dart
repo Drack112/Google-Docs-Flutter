4e4d6e4e098200b0b6ece78ca0152c15ba6a60a9
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-28 15:57:37 -0300

    :triangular_flag_on_post: remove debug_flag

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Google Docs Flutter",
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
