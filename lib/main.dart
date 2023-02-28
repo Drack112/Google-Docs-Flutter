f284343af673c2e06b2d11536fba4ddd129e2169
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-28 16:45:28 -0300

    :department_store: main application converter to a ConsumerStatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:docs_google/models/error_model.dart';
+import 'package:docs_google/repository/auth_repository.dart';
+import 'package:docs_google/screens/home_screen.dart';
 import 'package:docs_google/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -10,19 +13,42 @@ void main() {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  ErrorModel? errorModel;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserData();
+  }
+
+  void getUserData() async {
+    errorModel = await ref.read(authRepositoryProvider).getUserData();
+
+    if (errorModel != null && errorModel!.data != null) {
+      ref.read(userProvider.notifier).update((state) => errorModel!.data);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final user = ref.watch(userProvider);
+
     return MaterialApp(
       title: "Google Docs Flutter",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginScreen(),
+      home: user == null ? const LoginScreen() : const HomeScreen(),
     );
   }
 }
