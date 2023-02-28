83737f6f75416d92043dd80bf8219984880aaa88
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-28 16:00:12 -0300

    :small_blue_diamond: route to work with login

--- server/src/routes/AuthRoutes.ts
+++ server/src/routes/AuthRoutes.ts
@@ -1,5 +1,9 @@
 import User from "../schemas/user";
 import { Request, Response, Router } from "express";
+import { sign } from "jsonwebtoken";
+
+import authConfig from "../config/auth";
+import isAuthenticated from "../middlewares/isAuthenticated";
 
 const authRouter = Router();
 
@@ -17,13 +21,27 @@ authRouter.post("/api/signup", async (req: Request, res: Response) => {
     const newUser = await User.create({
       name,
       email,
-      profilePic,
+      profilePic
     })
 
-    return res.status(201).json(newUser)
+    const token = sign({}, authConfig.jwt.secret, {
+      subject: newUser.id,
+      expiresIn: authConfig.jwt.expiresIn,
+    });
+
+    return res.status(201).json({ user: newUser, token });
   } catch (err) {
-    return res.status(500).json({ error: err })
+    return res.status(500).send(err);
   }
+})
+
+authRouter.get("/", isAuthenticated, async (req: Request, res: Response) => {
+  const user = await User.findById(req.user)
+
+  const authHeader = req.headers.authorization;
+  const [, token] = authHeader.split(" ");
+
+  res.status(200).json({ user, token })
 })
 
 export default authRouter;
