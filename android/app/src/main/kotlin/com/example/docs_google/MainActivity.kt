7479540169e97580e2137ed6175308c96f9d269f
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-27 22:17:26 -0300

    :rocket: Initial Commit, setup flutter project

---
+++ android/app/src/main/kotlin/com/example/docs_google/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.docs_google
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:docs_google/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Google Docs Flutter",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
