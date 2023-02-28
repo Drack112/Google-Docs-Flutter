25d87110e1cfca94959623a1cfb80274a477163b
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-28 14:23:06 -0300

    :scroll: integrate repository with login_screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,20 +1,35 @@
 import 'package:docs_google/colors.dart';
 import 'package:docs_google/repository/auth_repository.dart';
+import 'package:docs_google/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class LoginScreen extends ConsumerWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
-  void signInWithGoogle(WidgetRef ref) {
-    ref.read(authRepositoryProvider).SignInWithGoogle();
+  void signInWithGoogle(WidgetRef ref, BuildContext context) async {
+    final sMessenger = ScaffoldMessenger.of(context);
+    final navigator = Navigator.of(context);
+    final errorModel =
+        await ref.read(authRepositoryProvider).signInWithGoogle();
+
+    if (errorModel.error == null) {
+      await ref.read(userProvider.notifier).update(
+            (state) => errorModel.data,
+          );
+      navigator.push(
+        MaterialPageRoute(
+          builder: (context) => const HomeScreen(),
+        ),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: ElevatedButton.icon(
-        onPressed: () => signInWithGoogle(ref),
+        onPressed: () => signInWithGoogle(ref, context),
         label: const Text(
           "Sign In with Google",
           style: TextStyle(
